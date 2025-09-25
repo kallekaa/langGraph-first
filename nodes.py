2ab257cbbaee2e5d5087b134f70322ac326fb430
kallekaa <kalle.kaarlela@gmail.com>
2025-09-25 21:25:18 +0300

    nodes

---
+++ nodes.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from langgraph.graph import MessageState
+from langgraph.prebuilt import ToolNode
+
+from react import llm, tools
+
+load_dotenv()
+
+SYSTEM_MESSAGE = """
+You are a helpful AI assistant that helps people with their questions.
+You have access to tools.
+"""
+
+def run_agent_reasoning(state: MessageState) -> MessageState:
+    """Runs the agent reasoning step."""
+
+    # if state.messages[-1].content == "stop":
+    #     return state.add_message("Agent stopped.")
+    response = llm.invoke([{"role": "system", "content": SYSTEM_MESSAGE}, *state["messages"]])
+
+    return {"messages": [response]}
+
+tool_node = ToolNode(tools)
